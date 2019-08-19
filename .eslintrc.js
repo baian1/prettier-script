216aa8099e8c8c7ae82635f8d6bb29d471f18be0
baian1 <chizhongnan1@gmail.com>
2019-08-19 16:38:25 +0800

    init

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true
+  },
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+      impliedStrict: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["prettier"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended"
+  ],
+  rules: {
+    indent: [2, 2, { SwitchCase: 1 }],
+    "no-console": 1,
+
+    "@typescript-eslint/indent": [1, 2, { SwitchCase: 1 }],
+    "@typescript-eslint/explicit-function-return-type": 0,
+    "@typescript-eslint/no-empty-interface": 0,
+    "@typescript-eslint/no-var-requires": 0,
+    "@typescript-eslint/member-delimiter-style": 0
+  }
+};
